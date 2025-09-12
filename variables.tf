c564cb56b976fd359b417d5336f1758989685c2f
Federico <fglmr95@gmail.com>
2025-09-12 11:57:30 -0300

    variables.tf implemented

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "app_name" {
+  description = "Unique name for the Container App and its associated resources."
+  type = string
+}
+
+variable "container_image" {
+  description = "Full URL of the Docker image in ACR (ej: miacr.azurecr.io/miapi:v1)." # Necesitamos subir la imagen a ACR para tener nuestra URL
+  type = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the Resource Group where the app will be deployed."
+  type = string
+}
+
+variable "container_app_environment_id" {
+  description = "ID of the shared Azure Container Apps Environment."
+  type = string
+}
+
+variable "container_port" {
+  description = "Port exposed by the API container."
+  type = number
+  default = 8000
+}
+
+variable "health_check_path" {
+  description = "Endpoint for the health probe."
+  type = string
+  default = "/health"
+}
+
+variable "cpu" {
+  description = "CPU allocated to the container (e.g., 0.25)."
+  type = number
+  default = 0.25
+}
+
+variable "memory" {
+  description = "Memory allocated to the container (e.g., '0.5Gi')."
+  type = string
+  default = "0.5Gi"
+}
