33bbc5cb4d1d21749fc0b5f13c539698a975aae7
Federico <fglmr95@gmail.com>
2025-09-12 12:13:06 -0300

    outputs.tf implemented

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+# Public URL where the API can be accessed.
+output "app_url" {
+  description = "Public URL to access the Container App"
+  value = "https://${azurerm_container_app.app.latest_revision_fqdn}" # Azure automatically generates a unique domain.
+}
+
+# Internal ID of the Container application.
+output "app_id" {
+  description = "ID of the created Container App"
+  value = azurerm_container_app.app.id
+}
+
+# Container App Name (for confirmation).
+output "app_name" {
+  description = "Name of the Container App"
+  value = azurerm_container_app.app.name
+}
+
+# Full FQDN (technical name of the URL).
+output "app_fqdn" {
+  description = "Fully Qualified Domain Name of the Container App"
+  value = azurerm_container_app.app.latest_revision_fqdn
+}
